c87e1be1f93e20bebddada9b7d14f04bd582524f
Одбаяр Батсайхан <HP237911@G302-CC0121.local>
2024-03-27 18:55:01 +0800

    a

--- src/graphql/resolvers/mutations/user-mutations.ts
+++ src/graphql/resolvers/mutations/user-mutations.ts
@@ -1,4 +1,4 @@
-import { createUser, updateUser, deleteUser } from "@/services/user-service";
+import { createUser, updateUser, deleteUser, deleteAll } from "@/services/user-service";
 import { UserCreateInput, UserUpdateInput } from "@/graphql/generated";
 import { Prisma } from "@prisma/client";
 
@@ -6,4 +6,5 @@ export const userMutations = {
   createUser: (_: unknown, { input }: { input: UserCreateInput }) => createUser(input),
   updateUser: (_: unknown, { input }: { input: Prisma.UserUpdateInput & { id: string } }) => updateUser(input),
   deleteUser: (_: unknown, { id }: { id: string }) => deleteUser(id),
+  deleteAll: () => deleteAll(),
 };
